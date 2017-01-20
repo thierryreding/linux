2d904ca5cce2e15f1749025cc4a9f1632ebcb485
Thierry Reding <treding@nvidia.com>
2017-01-20 17:21:45 +0100

    powerpc: Move to system-power framework
    Use the new system-power framework to power off the system instead of
    the pm_power_off() function.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- arch/powerpc/xmon/xmon.c
+++ arch/powerpc/xmon/xmon.c
@@ -25,6 +25,7 @@
 #include <linux/nmi.h>
 #include <linux/ctype.h>
 #include <linux/highmem.h>
+#include <linux/system-power.h>
 
 #include <asm/debugfs.h>
 #include <asm/ptrace.h>
@@ -1155,8 +1156,7 @@ static void bootcmds(void)
 	else if (cmd == 'h')
 		ppc_md.halt();
 	else if (cmd == 'p')
-		if (pm_power_off)
-			pm_power_off();
+		system_power_off();
 }
 
 static int cpu_cmd(void)
