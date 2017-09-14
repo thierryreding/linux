bf17060d46f7f21a59f8fefde4c066a8f9c0d67b
Thierry Reding <treding@nvidia.com>
2017-09-14 11:10:35 -0700

    Revert "usb: chipidea: dual-role experiments"
    This reverts commit b0563339463476fe41fd0f290583588f31b57488.

--- include/linux/usb/tegra_usb_phy.h
+++ include/linux/usb/tegra_usb_phy.h
@@ -65,7 +65,6 @@ struct tegra_usb_phy {
 	const struct tegra_xtal_freq *freq;
 	void __iomem *regs;
 	void __iomem *pad_regs;
-	int irq;
 	struct clk *clk;
 	struct clk *pll_u;
 	struct clk *pad_clk;
@@ -79,8 +78,6 @@ struct tegra_usb_phy {
 	bool is_ulpi_phy;
 	int reset_gpio;
 	struct reset_control *pad_rst;
-
-	struct work_struct work;
 };
 
 void tegra_usb_phy_preresume(struct usb_phy *phy);
