b6260d300cb3751f35c420cf7322a7fa16a32baa
Thierry Reding <treding@nvidia.com>
2017-10-04 17:27:51 +0200

    drm/tegra: Add reservation object support
    This adds a reservation object to buffer objects in preparation for
    implementing implicit fencing.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/plane.c
+++ drivers/gpu/drm/tegra/plane.c
@@ -94,6 +94,23 @@ const struct drm_plane_funcs tegra_plane_funcs = {
 	.format_mod_supported = tegra_plane_format_mod_supported,
 };
 
+int tegra_plane_prepare_fb(struct drm_plane *plane,
+			   struct drm_plane_state *state)
+{
+	struct dma_fence *fence;
+	struct tegra_bo *bo;
+
+	if ((plane->state->fb == state->fb) || !state->fb)
+		return 0;
+
+	/* XXX handle multi-planar formats */
+	bo = tegra_fb_get_plane(state->fb, 0);
+	fence = reservation_object_get_excl_rcu(bo->resv);
+	drm_atomic_set_fence_for_plane(state, fence);
+
+	return 0;
+}
+
 int tegra_plane_state_add(struct tegra_plane *plane,
 			  struct drm_plane_state *state)
 {
