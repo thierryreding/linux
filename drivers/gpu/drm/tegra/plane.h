d62276ea3e91dfc2e58f9201cbe3bbd152b549b5
Thierry Reding <treding@nvidia.com>
2017-11-09 13:11:40 +0100

    WIP: drm/tegra: Reference count planes
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/plane.h
+++ drivers/gpu/drm/tegra/plane.h
@@ -19,6 +19,9 @@ struct tegra_plane {
 	struct tegra_dc *dc;
 	unsigned int offset;
 	unsigned int index;
+
+	unsigned int usecount;
+	struct mutex lock;
 };
 
 struct tegra_cursor {
@@ -32,6 +35,12 @@ struct tegra_cursor {
 static inline struct tegra_plane *to_tegra_plane(struct drm_plane *plane)
 {
 	return container_of(plane, struct tegra_plane, base);
+}
+
+static inline void tegra_plane_init(struct tegra_plane *plane)
+{
+	mutex_init(&plane->lock);
+	plane->usecount = 0;
 }
 
 struct tegra_plane_legacy_blending_state {
@@ -73,5 +82,8 @@ int tegra_plane_format(u32 fourcc, u32 *format, u32 *swap);
 bool tegra_plane_format_is_yuv(unsigned int format, bool *planar);
 int tegra_plane_setup_legacy_state(struct tegra_plane *tegra,
 				   struct tegra_plane_state *state);
+
+void tegra_plane_get(struct tegra_plane *plane);
+void tegra_plane_put(struct tegra_plane *plane);
 
 #endif /* TEGRA_PLANE_H */
