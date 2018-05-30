87c8e39043e724946421c078ac2809da8f22b3cd
Thierry Reding <treding@nvidia.com>
2018-05-30 14:35:04 +0200

    drm/tegra: Destage ABI
    All remnants of the old ABI have been removed and the new ABI provides
    everything we need. Destage it so that userspace can officially start to
    use it.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/drm.c
+++ drivers/gpu/drm/tegra/drm.c
@@ -780,7 +780,6 @@ put:
 	return err;
 }
 
-#ifdef CONFIG_DRM_TEGRA_STAGING
 static int tegra_gem_create(struct drm_device *drm, void *data,
 			    struct drm_file *file)
 {
@@ -918,10 +917,8 @@ unlock:
 	mutex_unlock(&fpriv->lock);
 	return err;
 }
-#endif
 
 static const struct drm_ioctl_desc tegra_drm_ioctls[] = {
-#ifdef CONFIG_DRM_TEGRA_STAGING
 	DRM_IOCTL_DEF_DRV(TEGRA_GEM_CREATE, tegra_gem_create,
 			  DRM_UNLOCKED | DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(TEGRA_GEM_MMAP, tegra_gem_mmap,
@@ -932,7 +929,6 @@ static const struct drm_ioctl_desc tegra_drm_ioctls[] = {
 			  DRM_UNLOCKED | DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(TEGRA_SUBMIT, tegra_submit,
 			  DRM_UNLOCKED | DRM_RENDER_ALLOW),
-#endif
 };
 
 static const struct file_operations tegra_drm_fops = {
