44cd7eba0c3636461261b59b8f6416d954317e76
Thierry Reding <treding@nvidia.com>
2018-09-21 11:00:25 +0200

    WIP: drm/nouveau: Add support for GV11B
    Signed-off-by: Thierry Reding <treding@nvidia.com>

---
+++ drivers/gpu/drm/nouveau/nvkm/subdev/fb/gv11b.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 NVIDIA Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER(S) OR AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <subdev/mmu.h>
+
+#include "gf100.h"
+#include "ram.h"
+
+static void
+gv11b_fb_init(struct nvkm_fb *base)
+{
+	struct nvkm_device *device = base->subdev.device;
+	struct gf100_fb *fb = gf100_fb(base);
+	struct nvkm_mmu *mmu = device->mmu;
+
+	nvkm_info(&base->subdev, "> %s(base=%px)\n", __func__, base);
+
+	gm200_fb_init(base);
+
+	if (1) {
+		u32 *data = page_address(fb->r100c10_page);
+		u32 value;
+
+		nvkm_info(&fb->base.subdev, "sysmem: %pad\n", &fb->r100c10);
+
+		nvkm_info(&fb->base.subdev, "DATA[0]: %08x\n", data[0]);
+		data[0] = 0xdeadbeef;
+		nvkm_info(&fb->base.subdev, "DATA[0]: %08x\n", data[0]);
+
+		dma_sync_single_for_device(device->dev, fb->r100c10, SZ_64K,
+					   DMA_TO_DEVICE);
+
+		value = (fb->r100c10 >> 16) & 0xffffff;
+		value |= mmu->iommu_mask >> 16;
+
+		value |= 0x03000000; /* NCOH */
+		nvkm_wr32(device, 0x001700, value);
+
+		value = nvkm_rd32(device, 0x001700);
+		nvkm_info(&fb->base.subdev, "PRAM: %08x\n", value);
+
+		value = nvkm_rd32(device, 0x700000);
+		nvkm_info(&fb->base.subdev, "PRAM[0]: %08x\n", value);
+	}
+
+	nvkm_info(&base->subdev, "< %s()\n", __func__);
+}
+
+static const struct nvkm_fb_func
+gv11b_fb = {
+	.dtor = gf100_fb_dtor,
+	.oneinit = gf100_fb_oneinit,
+	.init = gv11b_fb_init,
+	.init_page = gm200_fb_init_page,
+	.intr = gf100_fb_intr,
+};
+
+int
+gv11b_fb_new(struct nvkm_device *device, int index, struct nvkm_fb **pfb)
+{
+	return gf100_fb_new_(&gv11b_fb, device, index, pfb);
+}
---
+++ drivers/gpu/drm/nouveau/nvkm/subdev/mmu/vmmgv11b.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 NVIDIA Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER(S) OR AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include "vmm.h"
+
+static const struct nvkm_vmm_func
+gv11b_vmm = {
+	.join = gv100_vmm_join,
+	.part = gf100_vmm_part,
+	.valid = gp100_vmm_valid,
+	.flush = gp100_vmm_flush,
+	.mthd = gp100_vmm_mthd,
+	.invalidate_pdb = gp100_vmm_invalidate_pdb,
+	.page = {
+		{ 47, &gp100_vmm_desc_16[4], NVKM_VMM_PAGE_Sxxx },
+		{ 38, &gp100_vmm_desc_16[3], NVKM_VMM_PAGE_Sxxx },
+		{ 29, &gp100_vmm_desc_16[2], NVKM_VMM_PAGE_Sxxx },
+		{ 21, &gp100_vmm_desc_16[1], NVKM_VMM_PAGE_SVxC },
+		{ 16, &gp100_vmm_desc_16[0], NVKM_VMM_PAGE_SVxC },
+		{ 12, &gp100_vmm_desc_12[0], NVKM_VMM_PAGE_SVHx },
+		{}
+	}
+};
+
+int
+gv11b_vmm_new(struct nvkm_mmu *mmu, bool managed, u64 addr, u64 size,
+	      void *argv, u32 argc, struct lock_class_key *key,
+	      const char *name, struct nvkm_vmm **pvmm)
+{
+	return gp100_vmm_new_(&gv11b_vmm, mmu, managed, addr, size,
+			      argv, argc, key, name, pvmm);
+}
