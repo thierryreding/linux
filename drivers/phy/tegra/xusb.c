260341442dbf980dd1b6adeb3185fa520764e5be
Thierry Reding <treding@nvidia.com>
2019-06-07 16:02:18 +0200

    phy: tegra: xusb: Do not output error on deferred probe
    Deferred probe is not an error condition, so avoid confusing the user
    with an error message. The situation typically resolves itself, and
    there are other mechanisms in place to warn about devices that aren't
    able to finish probing because they keep being deferred.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/phy/tegra/xusb.c
+++ drivers/phy/tegra/xusb.c
@@ -921,7 +921,10 @@ static int tegra_xusb_padctl_probe(struct platform_device *pdev)
 	err = devm_regulator_bulk_get(&pdev->dev, padctl->soc->num_supplies,
 				      padctl->supplies);
 	if (err < 0) {
-		dev_err(&pdev->dev, "failed to get regulators: %d\n", err);
+		if (err != -EPROBE_DEFER)
+			dev_err(&pdev->dev, "failed to get regulators: %d\n",
+				err);
+
 		goto remove;
 	}
 
