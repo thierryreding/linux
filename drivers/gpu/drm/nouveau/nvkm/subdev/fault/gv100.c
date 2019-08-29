09cfbb30f3397caf58db2b5ce5c5b5364870db66
Thierry Reding <treding@nvidia.com>
2019-08-29 14:46:25 +0200

    drm/nouveau: fault: Store aperture in fault information
    The fault information register contains data about the aperture that
    caused the failure. This can be useful in debugging aperture related
    programming bugs.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/fault/gv100.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/fault/gv100.c
@@ -138,6 +138,7 @@ gv100_fault_intr_fault(struct nvkm_fault *fault)
 	info.inst = ((u64)insthi << 32) | (info0 & 0xfffff000);
 	info.time = 0;
 	info.engine = (info0 & 0x000000ff);
+	info.aperture = (info0 & 0x00000c00) >> 10;
 	info.valid  = (info1 & 0x80000000) >> 31;
 	info.gpc    = (info1 & 0x1f000000) >> 24;
 	info.hub    = (info1 & 0x00100000) >> 20;
