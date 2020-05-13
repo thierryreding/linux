342ef3bba5d6ad5a61a1cb24c693792e5ad0c7f5
Thierry Reding <treding@nvidia.com>
2020-05-13 12:52:24 +0200

    arm64: tegra: Initial Tegra234 VDK support
    The NVIDIA Tegra234 VDK is a simulation platform for the Orin SoC. It
    supports a subset of the peripherals that will be available in the final
    chip and serves as a bootstrapping platform.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

---
+++ include/dt-bindings/clock/tegra234-clock.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2018-2019, NVIDIA CORPORATION. All rights reserved. */
+
+#ifndef DT_BINDINGS_CLOCK_TEGRA234_CLOCK_H
+#define DT_BINDINGS_CLOCK_TEGRA234_CLOCK_H
+
+/** @brief output of mux controlled by CLK_RST_CONTROLLER_CLK_SOURCE_SDMMC4 */
+#define TEGRA234_CLK_SDMMC4			123U
+/** @brief output of mux controlled by CLK_RST_CONTROLLER_CLK_SOURCE_UARTA */
+#define TEGRA234_CLK_UARTA			155U
+
+#endif
