9ed6458be8e758c8d261f382fbcba42e605a2182
Thierry Reding <treding@nvidia.com>
2020-06-12 11:00:50 +0200

    soc/tegra: pmc: Enable SW-triggered SCE wake event
    Tegra186 and Tegra194 have a wake event that can be triggered from the
    software running on the SCE. Enable these wake events so that the SCE
    firmware can wake the system if necessary.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/soc/tegra/pmc.c
+++ drivers/soc/tegra/pmc.c
@@ -3197,6 +3197,7 @@ static const struct tegra_wake_event tegra186_wake_events[] = {
 	TEGRA_WAKE_IRQ("pmu", 24, 209),
 	TEGRA_WAKE_GPIO("power", 29, 1, TEGRA186_AON_GPIO(FF, 0)),
 	TEGRA_WAKE_IRQ("rtc", 73, 10),
+	TEGRA_WAKE_IRQ("sce", 83, 19),
 };
 
 static const struct tegra_pmc_soc tegra186_pmc_soc = {
@@ -3330,6 +3331,7 @@ static const struct tegra_wake_event tegra194_wake_events[] = {
 	TEGRA_WAKE_IRQ("pmu", 24, 209),
 	TEGRA_WAKE_GPIO("power", 29, 1, TEGRA194_AON_GPIO(EE, 4)),
 	TEGRA_WAKE_IRQ("rtc", 73, 10),
+	TEGRA_WAKE_IRQ("sce", 83, 19),
 };
 
 static const struct tegra_pmc_soc tegra194_pmc_soc = {
