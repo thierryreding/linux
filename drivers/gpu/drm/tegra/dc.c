669cef16955d4e88a5f682c38aa1cf464c5d7923
Mikko Perttunen <mperttunen@nvidia.com>
2021-01-11 15:00:04 +0200

    gpu: host1x: Cleanup and refcounting for syncpoints
    Add reference counting for allocated syncpoints to allow keeping
    them allocated while jobs are referencing them. Additionally,
    clean up various places using syncpoint IDs to use host1x_syncpt
    pointers instead.
    
    Signed-off-by: Mikko Perttunen <mperttunen@nvidia.com>

--- drivers/gpu/drm/tegra/dc.c
+++ drivers/gpu/drm/tegra/dc.c
@@ -2129,7 +2129,7 @@ cleanup:
 		drm_plane_cleanup(primary);
 
 	host1x_client_iommu_detach(client);
-	host1x_syncpt_free(dc->syncpt);
+	host1x_syncpt_put(dc->syncpt);
 
 	return err;
 }
@@ -2154,7 +2154,7 @@ static int tegra_dc_exit(struct host1x_client *client)
 	}
 
 	host1x_client_iommu_detach(client);
-	host1x_syncpt_free(dc->syncpt);
+	host1x_syncpt_put(dc->syncpt);
 
 	return 0;
 }
