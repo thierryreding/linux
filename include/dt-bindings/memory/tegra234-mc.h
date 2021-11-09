cb59972644e57bb470edfc8bd58ba1f7e2af97c3
Thierry Reding <treding@nvidia.com>
2021-11-09 13:33:50 +0100

    dt-bindings: memory: Add Tegra234 EQOS memory clients
    Add the memory client definitions for the EQOS hardware found on
    Tegra234 SoCs.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- include/dt-bindings/memory/tegra234-mc.h
+++ include/dt-bindings/memory/tegra234-mc.h
@@ -10,6 +10,7 @@
 
 /* NISO1 stream IDs */
 #define TEGRA234_SID_SDMMC4	0x02
+#define TEGRA234_SID_EQOS	0x03
 #define TEGRA234_SID_BPMP	0x10
 
 /*
@@ -20,6 +21,10 @@
 #define TEGRA234_MEMORY_CLIENT_SDMMCRAB 0x63
 /* sdmmcd memory write client */
 #define TEGRA234_MEMORY_CLIENT_SDMMCWAB 0x67
+/* EQOS read client */
+#define TEGRA234_MEMORY_CLIENT_EQOSR 0x8e
+/* EQOS write client */
+#define TEGRA234_MEMORY_CLIENT_EQOSW 0x8f
 /* BPMP read client */
 #define TEGRA234_MEMORY_CLIENT_BPMPR 0x93
 /* BPMP write client */
